a96eea073ce2b03af06debacc5481ca9661b1bb7
wildanrivky <newildanr@gmail.com>
2026-04-11 07:15:46 +0700

    Add files via upload

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// Lindungi semua rute termasuk halaman utama
+const isProtectedRoute = createRouteMatcher(['/']);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (isProtectedRoute(request)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
